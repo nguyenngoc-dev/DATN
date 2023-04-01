2191473a055d11cf4d449b72302b397f8dda1653
nguyen ngoc <nguyenngoc@gmail.com>
2023-04-01 18:54:37 +0700

    biuld header

--- font_end/perfume/src/components/layout/TheHeader.vue
+++ font_end/perfume/src/components/layout/TheHeader.vue
@@ -5,8 +5,9 @@
                 <img class="header-top-left-img" src="../../assets/img/logo1.png" alt="">
             </div>
             <div class="header-top-between">
-                <input class="header-top-search" placeholder="Tìm kiếm sản phẩm">
+                <input class="header-top-search" placeholder="Tìm kiếm sản phẩm...">
                 <div class="icon-search"></div>
+                <i class="fa-solid fa-magnifying-glass"></i>
             </div>
             <div class="header-top-right">
                 <div class="header-top-right-first">
@@ -14,7 +15,11 @@
                         
                     </div>
                     <div class="header-top-right-first-left">
-                        <div class="header-top-right-first-left-text"></div>
+                        <div class="header-top-right-first-left-text">
+                                Hotline tư vấn
+                                <br>
+                                0333168440
+                        </div>
                         <div class="header-top-right-first-left-content">
 
                         </div>
@@ -25,9 +30,11 @@
                         
                     </div>
                     <div class="header-top-right-first-left">
-                        <div class="header-top-right-first-left-text"></div>
+                        <div class="header-top-right-first-left-text">
+                            Giỏ hàng
+                        </div>
                         <div class="header-top-right-first-left-content">
-
+                            Có 0 Sản phẩm
                         </div>
                     </div>
                 </div>
@@ -41,3 +48,6 @@ export default {
     
 }
 </script>
+<style scoped>
+@import "../../assets/font/fontawesome-5.15.1/css/all.min.css";
+</style>
